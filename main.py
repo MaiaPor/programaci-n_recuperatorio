c5f24bff03921d828cf9e79c0291c8cd8affbd40
Maia Portilla <portilla.maia@gmail.com>
2025-10-28 19:54:27 -0300

    comienzo

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Hola mundo")
+from recuperatorio_27_8 import *
+#Inicializamos el menù
+
+bandera = True
+
+while bandera:
+    print("··· Estacionamiento Patos Center ···")
+    print("1. Cargar vehiculo")
+    print("2. Mostrar lista completa")
+    print("3. Buscar vehiculo por patente")
+    print("4. Ordenar vehiculo por año")
+    print("5. Vehiculo con mas horas estacionado")
+    print("6. Vehiculo con menos horas estacionado")
+    print("7. Cantidad de vehiculos con mas de 4 horas estacionado")
+    print("8. Promedio de horas estacionadas")
+    print("9. Cantidad e vehiculos de la marca Ford")
+    print("0. Salir del menú")
+
+    eleccion = int(input("Seleccione la opción"))
+
+    if eleccion == 0:
+        bandera = False
+    elif eleccion == 1:
+        carga_de_datos()
--- recuperatorio_27_8.py
+++ recuperatorio_27_8.py
@@ -1,1 +1,36 @@
-print("Hola")
+#1.Cargar vehiculo
+
+def carga_de_datos(datos):
+    pass
+
+#2.Mostrar lista completa
+def carga_de_datos(datos):
+    pass
+
+#3.Buscar vehiculo por patente
+def carga_de_datos(datos):
+    pass
+
+#4.Ordenar vehiculos por año(descendente)
+def carga_de_datos(datos):
+    pass
+
+#5.Vehiculo con las horas estacionado
+def carga_de_datos(datos):
+    pass
+
+#6.Vehiculo con menos horas estacionado
+def carga_de_datos(datos):
+    pass
+
+#7.Cantidad de vehiculos con màs de 4 horas estacionado
+def carga_de_datos(datos):
+    pass
+
+#8.Promedio de horas estacionadas
+def carga_de_datos(datos):
+    pass
+
+#9.Cantidad de vehiculos de la marca "Ford"
+def carga_de_datos(datos):
+    pass
